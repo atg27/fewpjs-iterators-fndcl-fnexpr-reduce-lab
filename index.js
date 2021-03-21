303ded325f9176b639db028b2caef827a9876726
Andrew <atg27@uw.edu>
2021-03-21 16:35:13 -0700

    commit tree 72a9811c449ec84999aa4e81749a3b973bf75cf4

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+
+//const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// arr.reduce(callback( accumulator, currentValue, [, index[, array]] )[, initialValue])
+//console.log(array1.reduce(reducer));
